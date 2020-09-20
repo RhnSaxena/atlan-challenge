01a2e6e90936bf6569d6d928b1c6fb54d6c63709
Rohan Saxena <35458735+RhnSaxena@users.noreply.github.com>
2020-09-20 19:12:01 +0530

    feat(process_thread): added csv processing

--- api/process_thread.py
+++ api/process_thread.py
@@ -1,3 +1,9 @@
+from tempfile import NamedTemporaryFile
+import shutil
+import csv
+import time
+
+
 class Process:
     def __init__(self):
         self._running = True
@@ -7,7 +13,9 @@ class Process:
 
     def pause(self):
         self._running = False
-        return "Process paused."
+        return "Process paused. Completed {} on line no {}".format(
+            (self._current_row / self._total_row) * 100, self._current_row
+        )
 
     def resume(self):
         self._running = True
@@ -24,14 +32,31 @@ class Process:
             return "paused"
 
     def run(self, file):
-        self._current_row = 0
-        self._total_row = 100000
-        while not self._terminate:
-            print(self._current_row)
-            self._current_row = self._current_row + 1
-            while not self._running:
-                if self._terminate:
-                    break
-            if self._current_row > self._total_row:
-                self.terminate()
-        print("stopped")
+
+        tempfile = NamedTemporaryFile(mode="w", delete=False, newline="")
+        with file.read(), tempfile:
+            reader = csv.DictReader(file.stream)
+            self._current_row = 1
+            # self._total_row = 20000
+            self._total_row = len(reader)
+            fields = reader.fieldnames
+            fields = {fields[1]: fields[1], fields[0]: fields[0], fields[2]: fields[2]}
+            writer = csv.DictWriter(tempfile, fieldnames=fields)
+            writer.writerow(fields)
+
+            while not self._terminate:
+                print(reader[self._current_row])
+                writer.writerow(reader[self._current_row])
+                self._current_row = self._current_row + 1
+                time.sleep(0.10)
+                # self._current_row = self._current_row + 1
+                while not self._running:
+                    if self._terminate:
+                        break
+                if self._current_row >= self._total_row - 1:
+                    self.terminate()
+            print("stopped")
+
+        # filename = "employee_birthday.csv"
+
+        shutil.move(tempfile.name, file)
